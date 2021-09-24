c943ac33739a43a9aa65c9fefa8faf08fe384945
Humbert1983 <humberto.poblete1983@gmail.com>
2021-09-24 17:49:58 -0300

    Last Commit of Clase 15

---
+++ js/app.js
@@ -0,0 +1,19 @@
+console.log("Arranca la página...ahora desde un archivo js");
+
+//let
+var soyUnaVariable = "Un string con comillas 'Esto es un string' " ;
+
+var SoyUnNumeroEntero = 123131313;
+
+var SoyUnNuneroFlotante = 23.54;
+
+const ESTO_ES_UNA_CONSTANTE = "Soy una constante...";
+
+const PI = 3.141528;
+
+console.log(ESTO_ES_UNA_CONSTANTE + PI); // Usar log en la consola
+console.log(PI)
+
+console.dir(document.body); // Usar dir en la consola...
+
+
